e9232f1ff59befdeec363ff83da736a9ef7757d9
Nikkil <nikkil.kv@gmail.com>
2020-04-24 10:55:08 +0530

    TheFactorfactor app

--- app/src/main/java/com/example/thefactor/MainActivity.java
+++ app/src/main/java/com/example/thefactor/MainActivity.java
@@ -6,37 +6,24 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.VibrationEffect;
-import android.text.Editable;
-import android.text.Layout;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.os.Vibrator;
-import android.widget.Toast;
-
-
 import java.util.Random;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.Vector;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Vibrator v ;
     LinearLayout bg;
-    int number = 0 , max_streak ,current_Streak =0,prev_number;
-    int choice, counter,score =0;
-    boolean OptionChosen = false;
-
+    int number = 0 , max_streak ,current_Streak =0,prev_number,choice, counter,score =0;
+    boolean OptionChosen = true;
     Button optionA, optionB, optionC;
     TextView time, answer,scoreview,max_Streak,curr_Streak;
     EditText value;
@@ -52,30 +39,85 @@ public class MainActivity extends AppCompatActivity {
         optionA = (Button) findViewById(R.id.buttonA);
         optionB = (Button) findViewById(R.id.buttonB);
         optionC = (Button) findViewById(R.id.buttonC);
-
         scoreview = (TextView) findViewById(R.id.score);
         v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         max_Streak = (TextView) findViewById(R.id.max_streak);
         curr_Streak = (TextView) findViewById(R.id.curr_streak);
-
         value = (EditText) findViewById(R.id.value);
         time = (TextView) findViewById(R.id.time);
         answer = (TextView) findViewById(R.id.Answer);
         bg = (LinearLayout) findViewById(R.id.bg);
-
+        optionA.setVisibility(View.GONE);
+        optionB.setVisibility(View.GONE);
+        optionC.setVisibility(View.GONE);
     }
 
-    public void done(View view)
-    {
-        prev_number = number;
-        number = Integer.parseInt(value.getText().toString());
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean("MyBoolean", true);
+        savedInstanceState.putString("optionA", optionA.getText().toString());
+        savedInstanceState.putInt("counter", counter);
+        savedInstanceState.putString("MyString", "Welcome back to Android");
+    }
 
-        if(prev_number != number)
-        {
-            if(number > 0)
-            {
-                reset();
-                factors();
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        boolean myBoolean = savedInstanceState.getBoolean("MyBoolean");
+        optionA.setText(savedInstanceState.getString("optionA"));
+        counter = savedInstanceState.getInt("counter");
+        String myString = savedInstanceState.getString("MyString");
+        optionA.setVisibility(View.VISIBLE);
+        optionB.setVisibility(View.VISIBLE);
+        optionC.setVisibility(View.VISIBLE);
+    }
+
+
+    public void done(View view) {
+
+        optionA.setVisibility(View.VISIBLE);
+        optionB.setVisibility(View.VISIBLE);
+        optionC.setVisibility(View.VISIBLE);
+
+        prev_number = number;
+
+
+        if (OptionChosen == true) {
+            number = Integer.parseInt(value.getText().toString());
+            if (prev_number != number) {
+                counter = 0;
+                OptionChosen = false;
+
+                if (number > 0) {
+                    reset();
+                    value.setFocusable(false);
+                    factors();
+
+                }
+            }
+        }
+    }
+
+    public void random(View view)
+    {
+        optionA.setVisibility(View.VISIBLE);
+        optionB.setVisibility(View.VISIBLE);
+        optionC.setVisibility(View.VISIBLE);
+        Random rand = new Random();
+        prev_number = number;
+
+        if(OptionChosen == true) {
+            number =  rand.nextInt(500);
+            if (prev_number != number) {
+                counter = 0;
+                OptionChosen = false;
+                value.setText(Integer.toString(number));
+                if (number > 0) {
+                    reset();
+                    value.setFocusable(false);
+                    factors();
+                }
 
             }
         }
@@ -85,17 +127,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume()
     {
         super.onResume();
-
-        SharedPreferences sh
-                = getSharedPreferences("MySharedPref",
-                MODE_PRIVATE);
-
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         max_streak = sh.getInt("streak",0);
-
-        // Setting the fetched data
-        // in the EditTexts
         max_Streak.setText(Integer.toString(max_streak));
-
     }
 
 
@@ -103,96 +137,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause()
     {
         super.onPause();
-
-        SharedPreferences sharedPreferences
-                = getSharedPreferences("MySharedPref",
-                MODE_PRIVATE);
-        SharedPreferences.Editor myEdit
-                = sharedPreferences.edit();
-
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
         myEdit.putInt("streak", max_streak);
         myEdit.apply();
     }
 
 
     public void factors() {
-        Vector<Integer> factors = new Vector<>();
-        Vector<Integer> non_factors = new Vector<>();
-
-
-        for (int i = 1; i <= number; i++) {
-
-            if (number % i == 0)
-                factors.add(i);
-
-            else
-                non_factors.add(i);
-
-        }
-
-        if(number <= 6)
-        {
-            for(int i = 6;i<12;i++)
-                non_factors.add(i);
-        }
-
-
-
         Random rand = new Random();
         choice = rand.nextInt(3);
-
-        int temporary;
+        int temporary= rand.nextInt(number);
 
         if (choice == 0) {
-
-            temporary = factors.get(rand.nextInt(factors.size()));
+           while(number % temporary != 0)
+               temporary++;
             optionA.setText(Integer.toString(temporary));
         } else {
-            temporary = non_factors.get(rand.nextInt(non_factors.size()));
+            while(number % temporary == 0)
+                temporary++;
             optionA.setText(Integer.toString(temporary));
-            non_factors.removeElement(temporary);
         }
 
+        temporary= rand.nextInt(number);
 
         if (choice == 1) {
-
-            temporary = factors.get(rand.nextInt(factors.size()));
+            while(number % temporary != 0)
+            temporary++;
             optionB.setText(Integer.toString(temporary));
         } else {
-            temporary = non_factors.get(rand.nextInt(non_factors.size()));
+            while(number % temporary == 0)
+                temporary++;
             optionB.setText(Integer.toString(temporary));
-            non_factors.removeElement(temporary);
         }
 
-        if (choice == 2) {
+        temporary= rand.nextInt(number);
 
-            temporary = factors.get(rand.nextInt(factors.size()));
+        if (choice == 2) {
+            while(number % temporary != 0)
+                temporary++;
             optionC.setText(Integer.toString(temporary));
         } else {
-            temporary = non_factors.get(rand.nextInt(non_factors.size()));
+            while(number % temporary == 0)
+                temporary++;
             optionC.setText(Integer.toString(temporary));
-            non_factors.removeElement(temporary);
         }
 
         counter = 0;
          timer = new CountDownTimer(10000, 1000) {
             public void onTick(long millisUntilFinished) {
-                time.setText(String.valueOf(counter));
+                time.setText(String.valueOf(10 - counter));
                 counter++;
-
             }
-
-
             public void onFinish() {
                 counter = 0;
                 time.setText("TIME OUT!!!");
                 showResult();
             }
-
         }.start();
-
-
     }
+
 
     public void OPTION_A(View view) {
         if (!OptionChosen) {
@@ -322,6 +326,7 @@ public class MainActivity extends AppCompatActivity {
     {
         answer.setText("Final Score = " + score);
         OptionChosen=true;
+        value.setFocusableInTouchMode(true);
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
@@ -331,11 +336,13 @@ public class MainActivity extends AppCompatActivity {
                 score = 0;
                 scoreview.setText(" " + score);
 
-                value.setText("5");
+                value.setText(Integer.toString(number));
                 value.setHint("Enter a number ");
-                optionA.setText("A");
-                optionB.setText("B");
-                optionC.setText("C");
+                optionA.setVisibility(View.INVISIBLE);
+                optionB.setVisibility(View.INVISIBLE);
+                optionC.setVisibility(View.INVISIBLE);
+                value.setHint("Enter a number");
+                value.setText(null);
 
                 answer.setText(" ");
                 time.setText("0");
@@ -351,6 +358,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void background(final int ans)
     {
+        value.setFocusableInTouchMode(true);
 
         if(ans == 0)
         bg.setBackgroundColor(Color.parseColor("#FF0000"));
@@ -370,6 +378,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 
 }
 
